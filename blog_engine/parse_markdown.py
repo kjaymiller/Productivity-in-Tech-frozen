4a133005ecd0a2f356dd7ab4310c686ddaddd693
Jay Miller <kjaymiller@gmail.com>
2018-11-26 21:02:03 -0800

    new mailerlite newsletter slideover

--- blog_engine/parse_markdown.py
+++ blog_engine/parse_markdown.py
@@ -8,14 +8,34 @@ Step 3b - If 2 == True, Skip and go to the next file
 """
 
 from markdown import markdown
+from pathlib import Path
+import json
 
+content_libraries = ['podcasts', 'microblog']
 
 def render_post(md_content):
-    post = md_content.split('\n\n', 2)
-    metadata_string = post[0]
+    with open(md_content):
+        post = md_content.split('\n\n', 2)
+        metadata_string = post[0]
+    
     metadata = {}
+
     for line in metadata_string.split('\n'): 
-        line_data = line.split(':',2 )
+        line_data = line.split(': ',2 )
         metadata[line_data[0].lower()] = line_data[1]
+
     metadata['content'] = markdown(post[-1])
-    return metadata
+    return metadata     
+    
+
+def check_for_json_file(json_file):
+    if not Path(json_file).exists():
+        with open(json_file) as f:
+            return f.write('')
+
+
+def add_json_content(json_object, metadata):
+    return json_objects['items'].append(metadata)
+    
+    
+    
