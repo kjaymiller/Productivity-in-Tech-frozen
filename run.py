2c384420ca90ecc77f268242640629f386593deb
Kevin Miller <kmiller@w99x788i.waxie.com>
2019-02-04 16:17:05 -0800

    update PIT Backend

--- run.py
+++ run.py
@@ -36,7 +36,7 @@ def index():
                     image='fa-microphone-alt'),
                 Link(name='Youtube',
                     url='https://www.youtube.com/productivityintech',
-                    image='fa-youtube')
+                    image='fa-youtube-play')
                 ]
     return Page(template='index.html', services=services).html 
 
