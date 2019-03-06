cc66fbf517a9301af5d9059efe372756e1c4d7b1
Kevin Miller <kmiller@w99x788i.waxie.com>
2019-03-05 16:26:33 -0800

    tagging works

--- run.py
+++ run.py
@@ -58,20 +58,21 @@ def categorization():
                 Page(template='blog_list.html',
                     post_list=blog.categories[category], 
                     output_path=blog.output_path).html)
+        feed_gen(blog, page_filter=blog.categories[category], output_path=category_path, name=category)
     
     tag_path = Path(f'{blog.output_path}/tag')
     tag_path.mkdir(parents=True, exist_ok=True)
     write_page(f'{tag_path}/all.html', Page(template='categories.html',
         topic_list=[t for t in blog.tags]).html)
-
+        
     for tag in blog.tags:
         write_page(f'{tag_path}/{tag}.html',
                 Page(template='blog_list.html',
-                post_list=blog.categories[category],
+                post_list=blog.tags[tag],
                 output_path=blog.output_path).html)
-
     
 index()
 categorization()
 pagination()
 feed_gen(blog)
+
