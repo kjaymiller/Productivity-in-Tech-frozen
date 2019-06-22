a0ef8b1bb6ee168abf379eb4ad1bf621aa4afcc2
kjaymiller <kjaymiller@gmail.com>
2019-06-22 08:35:33 -0700

    mx-auto on options

--- content/pages/memberships.md
+++ content/pages/memberships.md
@@ -9,9 +9,9 @@ slug: memberships
 While the business does make some revenue from consulting, coaching and speaking, we cannot continue to grow the community and the services we offer without support from the community members like you! 
 </p>
 
-<div class="jumbotron bg-transparent border border-primary">
+<div class="col-md-6 my-3 text-centered mx-auto bg-transparent border border-primary">
 <h2 class="text-primary">Get access to:</h2>
-<div class="col-md-6 my-3 text-centered">
+<div class="">
 
 <ul class="list-group list-group-flush my-3">
 <li class="list-group-item">Priority Access to the PIT Mastermind Group</li>
--- run.py
+++ run.py
@@ -11,26 +11,26 @@ from links import Link
 
 engine = Engine()
 
-pages = Collection(
+# Add Collections
+pages = engine.add_collection(
         name='pages',
         content_type=Page,
         content_path='pages',
         )
-
-blog = Collection(name='blog',
+blog = engine.add_collection(
+        name='blog',
         content_type=BlogPost,
         output_path='blog',
         )
-
-services = Collection(
+services = enginve.add_collection(
         name='services',
         content_type=Page,
         output_path='services',
         content_path='services',
         )
 
-engine.collections = (pages, blog, services)
 
+# Build Static Pages
 @engine.build(Page, template='index.html', route='/index')
 def index():
     api_key = os.environ['BUTTONDOWN_API_KEY']
@@ -44,6 +44,7 @@ def index():
 
     return {'buttondown_count': results}
 
+
 @engine.build(
         Page,
         template='coaching/coaching_feedback.html',
@@ -51,6 +52,7 @@ def index():
         )
 def coaching_feedback():
     return {}
+
 
 @engine.build(
         Page,
@@ -60,10 +62,7 @@ def coaching_feedback():
 def podcasting_course():
     pass
 
-def pagination():
-    # TODO: Create Pagination
-    # write_paginated_pages(blog.name, blog.paginate, path=blog.output_path, template='blog_list.html')
-    pass
+
 
 
 if __name__ == "__main__":
