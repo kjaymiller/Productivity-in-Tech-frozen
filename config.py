65522fbefa409dcc77a042663f451c32921cd19b
kmiller <kjaymiller@gmail.com>
2019-07-15 10:32:37 -0700

    newsletter

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ DEFAULT_POST_BANNER = ''
 
 HEADER_LINKS = (
 #    Link(name='Blog', url='/blog/blog_0.html'),
-    Link(name='Newsletter', url='https://buttondown.email/productivityintech'),
+    Link(name='Newsletter', url='newsletter'),
     Link(name='Productivity in Tech Podcast',
         url='https://productivityintech.transistor.fm'),
     Link(name='PIT Membership', url='/memberships'),
