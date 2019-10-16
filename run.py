59b42483e11b61886aa077e312b687ca87280024
kmiller <kjaymiller@gmail.com>
2019-10-16 16:59:28 -0700

    reformat run

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from render_engine import Engine, Blog
+from render_engine import Site, Collection, Page
 from render_engine.links import Link
 
 import logging
@@ -17,43 +17,33 @@ HEADER_LINKS = (
 # Link(name="Courses", url="/dev-podcaster-course")
     )
 
-SITE = {'TITLE': 'Productivity in Tech'}
+class site(Site):
+    Title = 'Productivity in Tech'
+    HEADER_LINKS = HEADER_LINKS
 
-engine = Engine(
-        config='config.yaml',
-        HEADER_LINKS=HEADER_LINKS,
-        SITE=SITE,
-        strict=True,
-        )
 
 # Add Collections
-pages = engine.collection(
-        '/',
-        '/pages',
-        content_path='content/pages',
-        template='page.html',
-        name="Pages",
-        )
+@site.register_collection
+class Pages(Collection):
+    routes = ['/', '/pages']
+    content_path='content/pages'
+    template='page.html'
 
-blog = engine.collection(
-        '/blog',
-        content_path='content',
-        template='blog.html',
-        collection_object=Blog,
-        name="Blog",
-        index_template='archive.html',
-        )
 
-services = engine.collection(
-        '/',
-        '/services',
-        content_path='content/services',
-        template='page.html',
-        name='Services',
-        )
+@site.register_collection
+class Blog(Collection):
+    content_path='content',
+    template='blog.html',
 
-@engine.route( '/index', template='index.html')
-def index():
+
+@site.register_collection
+class Services(Collection):
+    routes = ['/', '/services']
+    content_path = 'content/services'
+    template = 'page.html'
+
+
+def get_subscriber_count():
     api_key = os.environ['BUTTONDOWN_API_KEY']
     headers = {'Authorization': f'Token {api_key}'}
     params = {'type': 'regular'}
@@ -63,25 +53,10 @@ def index():
     if r.status_code == 200:
         results = r.json()['count']
 
-#    return {}
-    return {'buttondown_count': results}
-
-# TODO Things like this should be a separate page
-@engine.route( '/dotnetcore', template='index.html')
-def index_dnetcore():
-    index_content = index()
-    index_content['promo'] = 'Join Jamie and many others in the PIT Family!'
-    index_content['promo_image'] = 'https://dotnetcore.show/content/images/2018/08/jamie-taylor-logo-podcast.svg'
-    return index_content
-
-# TODO Things like this should be a separate page
-@engine.route( '/developer-on-fire', template='index.html')
-def index_dev_on_fire():
-    index_content = index()
-    index_content['promo'] = 'Dave Rael trusts PIT to make him sound great!'
-    index_content['promo_image'] = 'https://s3-us-west-2.amazonaws.com/kjaymiller/images/developeronfire.png'
-    return index_content
-
-@engine.route( '/contact', template='contact.html')
+@site.route( '/contact', template='contact.html')
 def contact_page():
     return {}
+
+from .views.index import *
+
+site.render()
