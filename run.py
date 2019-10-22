9a9c4c498eaaf5d46c5dde5f68b2326d6922ffaf
kmiller <kjaymiller@gmail.com>
2019-10-22 16:22:20 -0700

    rewrite route

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from render_engine import Site, Collection
+from render_engine import Site, Collection, Page
 from render_engine.links import Link
 
 import os
@@ -24,21 +24,45 @@ site.HEADER_LINKS = HEADER_LINKS
 
 @site.register_collection
 class Pages(Collection):
-    routes = ['/', '/pages']
+    routes = ['', 'pages']
     content_path='content/pages'
     template = 'page.html'
 
 
 @site.register_collection
 class Blog(Collection):
+    routes = ['', 'blog']
     template = 'blog.html'
-    content_path='content',
+    content_path = 'content'
 
 
 @site.register_collection
 class Services(Collection):
-    routes = ['/', '/services']
+    routes = ['', '/services']
     content_path = 'content/services'
     template = 'page.html'
+
+@site.register_route
+class Index(Page):
+    template = 'index.html'
+
+@site.register_route
+class IndexDNetCore(Index):
+    index_content = {
+            'promo': 'Join Jamie and many others in the PIT Family!',
+            'promo_image': 'https://dotnetcore.show/content/images/2018/08/jamie-taylor-logo-podcast.svg',
+            }
+
+@site.register_route
+class IndexDevOnFire(Index):
+    index_content = {
+            'promo': 'Dave Rael trusts PIT to make him sound great!',
+            'promo_image': 'https://s3-us-west-2.amazonaws.com/kjaymiller/images/developeronfire.png',
+            }
+
+@site.register_route
+class ContactPage(Page):
+    slug='contact'
+    template='contact'
 
 site.render()
