0321c25d5810568b7ff0c8ba7d4f858266d0f903
kmiller <kjaymiller@gmail.com>
2019-10-23 13:52:44 -0700

    new archiving feature

--- views/collections.py
+++ views/collections.py
@@ -1,25 +1,24 @@
 from Site import site
-from render_engine.collection import Collection, Archive
+from render_engine.collection import Collection
 
 
 @site.register_collection
 class Pages(Collection):
-    routes = ['', 'pages']
-    content_path='content/pages'
-    template = 'page.html'
+    routes = ["", "pages"]
+    content_path = "content/pages"
+    template = "page.html"
 
 
 @site.register_collection
-class Blog(Collection, Archive):
-    routes = ['', 'blog']
-    template = 'blog.html'
-    content_path = 'content'
+class Blog(Collection):
+    archive = True
+    routes = ["", "blog"]
+    template = "blog.html"
+    content_path = "content"
 
 
 @site.register_collection
 class Services(Collection):
-    routes = ['', '/services']
-    content_path = 'content/services'
-    template = 'page.html'
-
-
+    routes = ["", "/services"]
+    content_path = "content/services"
+    template = "page.html"
--- views/index.py
+++ views/index.py
@@ -1,36 +1,36 @@
-from render_engine import Page
 from Site import site
+from render_engine import Page
 
 
 class Index:
-    template = 'index.html'
+    template = "index.html"
     index_content = {}
 
 
 @site.register_route
 class BaseIndex(Index, Page):
-    slug = 'index.html'
+    slug = "index.html"
 
 
 @site.register_route
 class IndexDNetCore(Index, Page):
-    slug = 'dotnetcore'
+    slug = "dotnetcore"
     index_content = {
-            'promo': 'Join Jamie and many others in the PIT Family!',
-            'promo_image': 'https://dotnetcore.show/content/images/2018/08/jamie-taylor-logo-podcast.svg',
-            }
+        "promo": "Join Jamie and many others in the PIT Family!",
+        "promo_image": "https://dotnetcore.show/content/images/2018/08/jamie-taylor-logo-podcast.svg",
+    }
 
 
 @site.register_route
 class IndexDevOnFire(Index, Page):
-    slug = 'devonfire'
+    slug = "devonfire"
     index_content = {
-            'promo': 'Dave Rael trusts PIT to make him sound great!',
-            'promo_image': 'https://s3-us-west-2.amazonaws.com/kjaymiller/images/developeronfire.png',
-            }
+        "promo": "Dave Rael trusts PIT to make him sound great!",
+        "promo_image": "https://s3-us-west-2.amazonaws.com/kjaymiller/images/developeronfire.png",
+    }
 
 
 @site.register_route
 class ContactPage(Page):
-    slug='contact'
-    template='contact.html'
+    slug = "contact"
+    template = "contact.html"
